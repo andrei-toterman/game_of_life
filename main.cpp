e47330889fce6d10ea121f373c01b5e789163879
Andrei Toterman <andrei.toterman@gmail.com>
2019-06-09 00:55:34 +0300

    Small tweaks

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@
 
 using namespace std;
 
-void print(SparseMatrix& m) {
+void print_mat(SparseMatrix& m) {
     for (int i = 0; i < m.get_rows(); ++i) {
-        for (int j = 0; j < m.get_cols(); ++j) {
+        for (int j = 0; j < m.get_cols(); ++j)
             cout << (m.element(i, j) ? 'O' : ' ') << ' ';
-        }
         cout << '|' << endl;
     }
     for (int i = 0; i < m.get_rows(); ++i)
@@ -27,42 +26,34 @@ int main() {
     char         cmd = 'r';
 
     while (cmd != 'q') {
-        switch (cmd) {
-            case 'q': break;
-            case 'r': {
-                gen.randomize();
-                break;
-            }
-            case 's': {
-                while (!kbhit()) {
-                    if (t < time(nullptr)) {
-                        t = time(nullptr);
-                        next_gen.clear();
-                        for (int i = 0; i < gen.get_rows(); ++i)
-                            for (int j = 0; j < gen.get_cols(); ++j) {
-                                int nbhs = gen.get_nbh_count(i, j);
-                                int cell = gen.element(i, j);
-                                if (cell == 0 && nbhs == 3)
-                                    next_gen.modify(i, j, 1);
-                                else if (cell == 1 && (nbhs < 2 || nbhs > 3))
-                                    next_gen.modify(i, j, 0);
-                                else
-                                    next_gen.modify(i, j, cell);
-                            }
-                        system("cls");
-                        gen = next_gen;
-                        print(next_gen);
-                        cout << endl << "press any key to stop" << endl;
-                    }
+        if (cmd == 'r') gen.randomize();
+        if (cmd == 's') {
+            while (!kbhit()) {
+                if (t < time(nullptr)) {
+                    t = time(nullptr);
+                    next_gen.clear();
+                    for (int i = 0; i < gen.get_rows(); ++i)
+                        for (int j = 0; j < gen.get_cols(); ++j) {
+                            int nbhs = gen.get_nbh_count(i, j);
+                            int cell = gen.element(i, j);
+                            if (cell == 0 && nbhs == 3)
+                                next_gen.modify(i, j, 1);
+                            else if (cell == 1 && (nbhs < 2 || nbhs > 3))
+                                next_gen.modify(i, j, 0);
+                            else
+                                next_gen.modify(i, j, cell);
+                        }
+                    system("cls");
+                    gen = next_gen;
+                    print_mat(next_gen);
+                    cout << endl << "press any key to stop" << endl;
                 }
-                break;
             }
         }
         system("cls");
-        print(gen);
+        print_mat(gen);
         cout << endl << "press S to start, R to reset or Q to quit" << endl;
         cin >> cmd;
     }
-
     return 0;
 }
